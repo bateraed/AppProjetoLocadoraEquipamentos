bd3e208a3005b9799cf26af2d49415ef0fc81b49
bateraed <bateraed@gmail.com>
2016-09-22 17:03:27 -0300

    Designer melhorado e código melhorado.

--- app/src/main/java/ed/com/br/appprojetolocadoraequipamentos/Util/DatabaseUtil.java
+++ app/src/main/java/ed/com/br/appprojetolocadoraequipamentos/Util/DatabaseUtil.java
@@ -20,18 +20,44 @@ public class DatabaseUtil extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        StringBuilder stringBuilderCreatetable = new StringBuilder();
+        StringBuilder stringBuilderCreateTabelaReserva = new StringBuilder();
 
-        stringBuilderCreatetable.append("CREATE TABLE tb_reserva (");
-        stringBuilderCreatetable.append("       id_reserva INTEGER PRIMARY KEY AUTOINCREMENT, ");
-        stringBuilderCreatetable.append("       nomeProfessor TEXT NOT NULL, ");
-        stringBuilderCreatetable.append("       equipamento TEXT NOT NULL, ");
-        stringBuilderCreatetable.append("       sala TEXT NOT NULL, ");
-        stringBuilderCreatetable.append("       data TEXT NOT NULL, ");
-        stringBuilderCreatetable.append("       horarioInicial TEXT NOT NULL, ");
-        stringBuilderCreatetable.append("       horarioFinal TEXT NOT NULL);");
+        stringBuilderCreateTabelaReserva.append("CREATE TABLE tb_reserva (");
+        stringBuilderCreateTabelaReserva.append("       id_reserva INTEGER PRIMARY KEY AUTOINCREMENT, ");
+        stringBuilderCreateTabelaReserva.append("       nomeProfessor TEXT NOT NULL, ");
+        stringBuilderCreateTabelaReserva.append("       equipamento TEXT NOT NULL, ");
+        stringBuilderCreateTabelaReserva.append("       sala TEXT NOT NULL, ");
+        stringBuilderCreateTabelaReserva.append("       data TEXT NOT NULL, ");
+        stringBuilderCreateTabelaReserva.append("       horarioInicial TEXT NOT NULL, ");
+        stringBuilderCreateTabelaReserva.append("       horarioFinal TEXT NOT NULL);");
 
-        db.execSQL(stringBuilderCreatetable.toString());
+        db.execSQL(stringBuilderCreateTabelaReserva.toString());
+
+        StringBuilder stringBuilderCreateTabelaProfessor = new StringBuilder();
+
+        stringBuilderCreateTabelaProfessor.append("CREATE TABLE tb_reserva (");
+        stringBuilderCreateTabelaProfessor.append("       id_reserva INTEGER PRIMARY KEY AUTOINCREMENT, ");
+        stringBuilderCreateTabelaProfessor.append("       nomeProfessor TEXT NOT NULL, ");
+        stringBuilderCreateTabelaProfessor.append("       equipamento TEXT NOT NULL, ");
+        stringBuilderCreateTabelaProfessor.append("       sala TEXT NOT NULL, ");
+        stringBuilderCreateTabelaProfessor.append("       data TEXT NOT NULL, ");
+        stringBuilderCreateTabelaProfessor.append("       horarioInicial TEXT NOT NULL, ");
+        stringBuilderCreateTabelaProfessor.append("       horarioFinal TEXT NOT NULL);");
+
+        db.execSQL(stringBuilderCreateTabelaProfessor.toString());
+
+        StringBuilder stringBuilderCreateTabelaEquipamento = new StringBuilder();
+
+        stringBuilderCreateTabelaEquipamento.append("CREATE TABLE tb_reserva (");
+        stringBuilderCreateTabelaEquipamento.append("       id_reserva INTEGER PRIMARY KEY AUTOINCREMENT, ");
+        stringBuilderCreateTabelaEquipamento.append("       nomeProfessor TEXT NOT NULL, ");
+        stringBuilderCreateTabelaEquipamento.append("       equipamento TEXT NOT NULL, ");
+        stringBuilderCreateTabelaEquipamento.append("       sala TEXT NOT NULL, ");
+        stringBuilderCreateTabelaEquipamento.append("       data TEXT NOT NULL, ");
+        stringBuilderCreateTabelaEquipamento.append("       horarioInicial TEXT NOT NULL, ");
+        stringBuilderCreateTabelaEquipamento.append("       horarioFinal TEXT NOT NULL);");
+
+        db.execSQL(stringBuilderCreateTabelaEquipamento.toString());
     }
 
     @Override
